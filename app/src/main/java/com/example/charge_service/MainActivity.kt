42838052b9a6d3363a724b2c19822e10edb0c262
0-x-14 <0x14@daum.net>
2023-11-20 23:59:19 +0900

    주영 xml 구성

---
+++ app/src/main/java/com/example/charge_service/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.charge_service
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.GravityCompat
+import com.example.charge_service.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.menuBtn.setOnClickListener {
+            binding.homeLayout.openDrawer(GravityCompat.END)
+        }
+    }
+}
