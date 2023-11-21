8899988231116294d2057e9981e793f5d785b51f
hyejin <hyejin66@sookmyung.ac.kr>
2023-11-22 00:25:26 +0900

    second try

--- app/src/main/java/com/example/charge_service/MainActivity.kt
+++ app/src/main/java/com/example/charge_service/MainActivity.kt
@@ -2,17 +2,26 @@ package com.example.charge_service
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import androidx.core.view.GravityCompat
 import com.example.charge_service.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.view1)
 
-        binding.menuBtn.setOnClickListener {
-            binding.homeLayout.openDrawer(GravityCompat.END)
-        }
     }
 }
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        val binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        binding.menuBtn.setOnClickListener {
+//            binding.homeLayout.openDrawer(GravityCompat.END)
+//        }
+//    }
+//}
+
