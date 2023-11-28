5730a8b46b7dce469a1000f2c7fe13c717f617ba
0-x-14 <81631374+0-x-14@users.noreply.github.com>
2023-11-28 23:05:34 +0900

    지도 API 중간수정

--- app/src/main/java/com/example/charge_service/MapsFragment.kt
+++ app/src/main/java/com/example/charge_service/MapsFragment.kt
@@ -2,7 +2,9 @@ package com.example.charge_service
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
@@ -14,6 +16,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
@@ -23,19 +26,30 @@ import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsFragment(val activity: Activity) : Fragment(), OnMapReadyCallback {
+class MapsFragment(var activity: Activity) : Fragment(), OnMapReadyCallback {
     lateinit var locationPermission: ActivityResultLauncher<Array<String>>
 
     private lateinit var gMap: GoogleMap
+    // 현재 위치를 검색하기 위해
 
     lateinit var fusedLocationClient: FusedLocationProviderClient
+    // 위치값 사용
     lateinit var locationCallback: LocationCallback
+    // 위치값 요청에 대한 정보
+
+    private lateinit var mapView: MapView
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        activity = context as Activity
+    } // fragment가 activity와 연결될 때 호출
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,78 +59,64 @@ class MapsFragment(val activity: Activity) : Fragment(), OnMapReadyCallback {
         locationPermission = registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions()
         ) { results ->
-            if (results.all { it.value }) {
-                updateLocation()
-                // 권한이 모두 허용된 경우에만 위치 정보를 가져오는 함수 호출
-            }
-            else {
-                Toast.makeText(activity, "권한 승인이 필요합니다.", Toast.LENGTH_SHORT).show()
-                // 권한이 하나라도 거부된 경우 경우 toast 메세지 출력
+            if (!results.all { it.value }) {
+                Toast.makeText(activity, "권한 승인이 필요합니다.", Toast.LENGTH_LONG).show()
             }
         }
 
-        locationPermission.launch(
-            arrayOf(
-                Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
-            )
-        )
-        return inflater.inflate(R.layout.fragment_maps, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(this)
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        gMap = googleMap
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity)
-        updateLocation() // 현재 위치 정보를 계속해서 불러오는 함수
-    }
-
-    fun updateLocation() {
-        // 필요한 위치 권한이 부여되었는지 확인
-        if (ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            val locationRequest = LocationRequest.create().apply {
-                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-                interval = 1000
-            }
-
-            locationCallback = object : LocationCallback() {
-                override fun onLocationResult(locationResult: LocationResult?) {
-                    locationResult?.let {
-                        for (location in it.locations) {
-                            Log.d("jupy", "현재 위치 위도 ${location.latitude} 경도 ${location.longitude}")
-                            setLastLocation(location.latitude, location.longitude)
-                            // 로그창에 위도 및 경도 출력, setLastLocation 호출로 최신 위치 갱신
-                        }
-                    }
-                }
-            }
-            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
-        } else {
-            // 권한이 없는 경우 사용자에게 권한을 요청
-            requestLocationPermission()
-        }
-    }
-    private fun requestLocationPermission() {
+        //권한 요청
         locationPermission.launch(
             arrayOf(
                 Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_FINE_LOCATION
             )
         )
+
+        return inflater.inflate(R.layout.fragment_maps, container, false)
     }
-    fun setLastLocation(latitude:Double, longitude:Double) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        gMap = googleMap
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity)
+        updateLocation()
+    }
+
+    fun updateLocation() {
+        val locationRequest = LocationRequest.create()
+        locationRequest.run {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 1000
+        }
+
+        locationCallback = object : LocationCallback() {
+            //1초에 한번씩 변경된 위치 정보가 onLocationResult 으로 전달된다.
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult?.let {
+                    for (location in it.locations) {
+                        Log.d("위치정보", "위도: ${location.latitude} 경도: ${location.longitude}")
+                        setLastLocation(location.latitude, location.longitude
+                        )
+                    }
+                }
+            }
+        }
+        //권한 처리
+        // fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+
+
+        // 필요한 위치 권한이 부여되었는지 확인
+    }
+
+    fun setLastLocation(latitude: Double, longitude:Double) {
         val LATLNG = LatLng(latitude, longitude)
 
-        val makerOptions = MarkerOptions().position(LATLNG).title("현재 위치")
-
+        val makerOptions = MarkerOptions().position(LATLNG).title("Here")
         val cameraPosition = CameraPosition.Builder().target(LATLNG).zoom(15.0f).build()
 
         gMap.clear()
@@ -124,5 +124,38 @@ class MapsFragment(val activity: Activity) : Fragment(), OnMapReadyCallback {
         gMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
 
         // 현재 위치 표시
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        // fragment가 화면에서 사라질 때 위치 업데이트 중지
+        if (::locationCallback.isInitialized) {
+            fusedLocationClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
