9692a3b8089d90d0755c993ff302f514e46f73d7
hyejin <hyejin66@sookmyung.ac.kr>
2023-12-18 04:07:05 +0900

    네이버 맵 구현

--- app/src/main/java/com/example/charge_service/MapsFragment.kt
+++ app/src/main/java/com/example/charge_service/MapsFragment.kt
@@ -3,8 +3,6 @@ package com.example.charge_service
 import android.Manifest
 import android.app.Activity
 import android.content.pm.PackageManager
-import androidx.fragment.app.Fragment
-
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
@@ -15,15 +13,14 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -60,6 +57,7 @@ class MapsFragment(val activity: Activity) : Fragment() {
                 Toast.makeText(activity, "권한 승인이 필요합니다.", Toast.LENGTH_SHORT).show()
                 // 권한이 하나라도 거부된 경우 경우 toast 메세지 출력
             }
+
         }
 
         locationPermission.launch(
