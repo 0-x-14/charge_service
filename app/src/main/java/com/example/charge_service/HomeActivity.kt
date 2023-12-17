975da55ccc479a8eff2f1ff61afe1c17531bea67
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2023-12-18 04:49:20 +0900

    HomeActivity 수정

--- app/src/main/java/com/example/charge_service/HomeActivity.kt
+++ app/src/main/java/com/example/charge_service/HomeActivity.kt
@@ -15,6 +15,8 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.example.charge_service.MainActivity.Companion.preferences
 import com.example.charge_service.databinding.ActivityMainBinding
 import com.example.charge_service.databinding.HomeBinding
 import com.example.charge_service.databinding.MainHeaderBinding
@@ -25,50 +27,48 @@ import com.google.android.material.navigation.NavigationView
 class HomeActivity: AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
     private val HomeFragment by lazy { HomeFragment() }
-    private val ReturnFragment by lazy {ReturnFragment()}
-    private val PayFragment by lazy {PayFragment()}
-    private val RentalConditionFragment by lazy {RentalConditionFragment()}
+    private val ReturnFragment by lazy { ReturnFragment() }
+    private val PayFragment by lazy { PayFragment() }
+    private val RentalConditionFragment by lazy { RentalConditionFragment() }
     //private vmeBinding.inflate(layoutInflater) }
-    private lateinit var preferences: al binding by lazy { HoPreferenceUtil
 
-    private val HomeUsingFragment by lazy {HomeUsingFragment()}
-    private val RentalCompFragment by lazy {RentalCompFragment()}
-    private val ReturnCompFragment by lazy {ReturnCompFragment()}
+    private val HomeUsingFragment by lazy { HomeUsingFragment() }
+    private val RentalCompFragment by lazy { RentalCompFragment() }
+    private val ReturnCompFragment by lazy { ReturnCompFragment() }
     // 각각 대여/반납 완료 화면이 정상적으로 구현되었는지 확인할 때 사용하는 용도
 
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navigationView: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
 
-    // navigationViewHeader 초기화
-    preferences = PreferenceUtil(applicationContext)
-    val binding = ActivityMainBinding.inflate(layoutInflater)
-    setContentView(R.layout.activity_main)
-    drawerLayout = binding.homeLayout
-    navigationView = binding.homeNavigation
-    super.onCreate(savedInstanceState)
+        // navigationViewHeader 초기화
+        preferences = PreferenceUtil(applicationContext)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+        drawerLayout = binding.homeLayout
+        navigationView = binding.homeNavigation
+        super.onCreate(savedInstanceState)
 
-    if (savedInstanceState == null) {
-        setUpBottomNavigationBar()
-    }
-
-    //로그인 후 id값을 받아와 사용자명 변경
-    val id = preferences.getString("id", "") ?: ""
-    val userName = findViewById<TextView>(R.id.userName)
-    if (userName != null) {
-        userName.setText("${id}님 안녕하세요")
-    }
-
-    val open = findViewById<ImageView>(R.id.menu_btn)
-    open.setOnClickListener {
-        val drawer = findViewById<DrawerLayout>(R.id.homeLayout)
-        if (!drawer.isDrawerOpen(GravityCompat.END)) {
-            drawer.openDrawer(GravityCompat.END)
+        if (savedInstanceState == null) {
+            setUpBottomNavigationBar()
         }
-    }
+
+        //로그인 후 id값을 받아와 사용자명 변경
+        val id = preferences.getString("id", "") ?: ""
+        val userName = findViewById<TextView>(R.id.userName)
+        if (userName != null) {
+            userName.setText("${id}님 안녕하세요")
+        }
+
+        val open = findViewById<ImageView>(R.id.menu_btn)
+        open.setOnClickListener {
+            val drawer = findViewById<DrawerLayout>(R.id.homeLayout)
+            if (!drawer.isDrawerOpen(GravityCompat.END)) {
+                drawer.openDrawer(GravityCompat.END)
+            }
+        }
 
     }
-
     private fun setUpBottomNavigationBar() {
         bottomNavigationView = findViewById(R.id.Smenu)
         bottomNavigationView.run {
@@ -76,9 +76,7 @@ class HomeActivity: AppCompatActivity() {
                 when(it.itemId) {
                     R.id.navi_home ->
                     { changeFragment(HomeFragment)
-                    {
-                        changeFragment(HomeUsingFragment)
-                    true
+                        true
                     }
                     R.id.navi_rental -> {
                         // changeFragment(RentalCompFragment)
@@ -103,11 +101,10 @@ class HomeActivity: AppCompatActivity() {
         }
     }
 
-
-    fun changeFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.navi_fragment_container, fragment)
-            .commit()
-    }
+        fun changeFragment(fragment: Fragment) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.navi_fragment_container, fragment)
+                .commit()
+        }
 }
