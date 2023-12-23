9a12f88f9c216466f48a0b15b7af8e69a54262ef
hyejin <hyejin66@sookmyung.ac.kr>
2023-12-23 21:44:06 +0900

    알림 부분 추가+ 오류 수정

---
+++ app/src/main/java/com/example/charge_service/AlarmActivity.kt
@@ -0,0 +1,39 @@
+package com.example.charge_service
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.charge_service.databinding.AlarmRecordBinding
+
+class AlarmActivity : AppCompatActivity() {
+
+    private lateinit var binding: AlarmRecordBinding
+    private lateinit var adapter: AlarmAdapter
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // xml과 연결
+        binding = AlarmRecordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 레이아웃매니저 적용 - 오래된게 아래에 가게 reverse로 설정
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.reverseLayout = true
+        layoutManager.stackFromEnd = true
+        binding.recview.layoutManager = layoutManager
+
+        val data = arrayListOf<String>("2023.12.01에 13:20 ~ 15:00까지 사용",
+            "대여가 완료되었습니다.\n2시간 뒤에 반납해주세요.", "2023.12.01에\n15:10 ~ 16:55까지 사용", "대여가 완료되었습니다.\n2시간 뒤에 반납해주세요.", "2023.12.04에\n18:20 ~ 20:00까지 사용"
+            , "대여가 완료되었습니다.\n2시간 뒤에 반납해주세요.", "2023.12.05에\n19:20 ~ 21:00까지 사용", "대여가 완료되었습니다.\n2시간 뒤에 반납해주세요.", "2023.12.06에\n09:10 ~ 11:00까지 사용",
+            "대여가 완료되었습니다.\n2시간 뒤에 반납해주세요.", "2023.12.07에\n09:30 ~ 10:00까지 사용", "대여가 완료되었습니다.\n2시간 뒤에 반납해주세요.", "2023.12.08에\n09:50 ~ 11:00까지 사용",
+            "대여가 완료되었습니다.\n2시간 뒤에 반납해주세요.", "2023.12.11에\n17:37 ~ 19:04까지 사용","대여가 완료되었습니다.\n2시간 뒤에 반납해주세요.","2023.12.12에\n21:10 ~ 22:00까지 사용")
+        // 어댑터 초기화 및 설정
+
+        // data.add("data test")
+        adapter = AlarmAdapter(data)
+        binding.recview.adapter = adapter
+
+    }
+}
--- app/src/main/java/com/example/charge_service/RentalFragment.kt
+++ app/src/main/java/com/example/charge_service/RentalFragment.kt
@@ -1,13 +1,19 @@
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Switch
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
 import com.example.charge_service.R
 import com.google.firebase.database.FirebaseDatabase
@@ -25,6 +31,10 @@ class RentalFragment : Fragment() {
     private lateinit var textView1: TextView
     private lateinit var textView2: TextView
     private lateinit var textView3: TextView
+    private val NOTIFICATION_CHANNEL_ID = "rental_notification_channel"
+    private val channelName = "충전하겠숙?"
+    private val description = "대여가 완료되었습니다. 2시간 뒤에 반납해주세요."
+    private val importance = NotificationManager.IMPORTANCE_HIGH
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,6 +43,14 @@ class RentalFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.rental, container, false)
         database = FirebaseDatabase.getInstance()
+
+        val toggle8 = view.findViewById<Switch>(R.id.eightpin_toggle)
+        val toggleC = view.findViewById<Switch>(R.id.eightpin_toggle2)
+        val toggleNote = view.findViewById<Switch>(R.id.eightpin_toggle3)
+
+        val eightPin = view.findViewById<TextView>(R.id.numOfEightPin)
+        val cPin = view.findViewById<TextView>(R.id.numOfCtype)
+        val notebook = view.findViewById<TextView>(R.id.numOfnote)
 
         rentalButton1 = view.findViewById(R.id.rentalButton1)
         rentalButton2 = view.findViewById(R.id.rentalButton2)
@@ -45,8 +63,15 @@ class RentalFragment : Fragment() {
             startQRScanner(1)
             if(textView1.text.toString().toInt() > 0){
                 textView1.text = (textView1.text.toString().toInt() - 1).toString()
+<<<<<<< HEAD
             }
             else{
+=======
+                saveToSharedPreferences("numOfEightPin", textView1.text.toString())
+                showNotification()
+            }
+            else {
+>>>>>>> b9a2b64 (알림 부분 추가+ 오류 수정)
                 Toast.makeText(requireContext(), "8핀 충전기가 모두 대여되었습니다.", Toast.LENGTH_LONG).show()
             }
         }
@@ -55,23 +80,75 @@ class RentalFragment : Fragment() {
             startQRScanner(2)
             if(textView2.text.toString().toInt() > 0){
                 textView2.text = (textView2.text.toString().toInt() - 1).toString()
+<<<<<<< HEAD
             }
             else{
+=======
+                saveToSharedPreferences("numOfCtype", textView2.text.toString())
+                showNotification()
+            }
+            else {
+>>>>>>> b9a2b64 (알림 부분 추가+ 오류 수정)
                 Toast.makeText(requireContext(), "C타입 충전기가 모두 대여되었습니다.", Toast.LENGTH_LONG).show()
             }
         }
 
         rentalButton3.setOnClickListener {
             startQRScanner(3)
+<<<<<<< HEAD
            if(textView3.text.toString().toInt() > 0){
                textView3.text = (textView3.text.toString().toInt() - 1).toString()
            }
             else{
+=======
+            if (textView3.text.toString().toInt() > 0) {
+                textView3.text = (textView3.text.toString().toInt() - 1).toString()
+                saveToSharedPreferences("numOfnote", textView3.text.toString())
+                showNotification()
+            }
+            else {
+>>>>>>> b9a2b64 (알림 부분 추가+ 오류 수정)
                 Toast.makeText(requireContext(), "노트북 충전기가 모두 대여되었습니다.", Toast.LENGTH_LONG).show()
             }
         }
 
         return view
+
+    }
+    private fun showNotification() {
+        // 1. Notification Channel 생성 (API Level 26 이상)
+        createNotificationChannel()
+
+        // 2. Notification Builder 사용
+        val builder = NotificationCompat.Builder(requireContext(), NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.alarm_img)
+            .setContentTitle(channelName)
+            .setContentText(description)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+
+        // 3. Notification Manager로 Notification 표시
+        val notificationManager =
+            requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.notify(1234, builder.build())
+    }
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                channelName,
+                importance
+            ).apply {
+                description = this@RentalFragment.description
+                enableLights(true)
+            }
+
+            val notificationManager =
+                requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     private fun startQRScanner(buttonId: Int) {
@@ -115,6 +192,7 @@ class RentalFragment : Fragment() {
                 Toast.makeText(requireContext(), "시간을 저장하는 데 실패했습니다.", Toast.LENGTH_SHORT).show()
             }
     }
+<<<<<<< HEAD
 
 
 <<<<<<< HEAD
@@ -123,3 +201,9 @@ class RentalFragment : Fragment() {
 =======
 }
 >>>>>>> 36e671c (QR 스캔 처리 갯수 업데이트 되는거까지 구현 완료)
+=======
+    fun requestSharedPreferencesReset() {
+        (activity as? HomeActivity)?.resetSharedPreferences()
+    }
+}
+>>>>>>> b9a2b64 (알림 부분 추가+ 오류 수정)
